ab466b747e4fe007d50f10841bbcb6ca4ba16c03
MarcMaugerZwift <marc-mauger@zwift.com>
2022-12-03 14:15:59 -0800

    tests pass. very verbose syntax for tree structure

--- src/group.rs
+++ src/group.rs
@@ -4,51 +4,50 @@ use crate::matrix::Matrix4;
 use crate::ray::Ray;
 use crate::shape::*;
 use crate::tuple::*;
-use std::{cell::RefCell, rc::Rc, sync::Weak};
+use std::{
+    cell::RefCell,
+    rc::Rc,
+    sync::{Arc, Weak},
+};
 
-type GroupRef<T> = Rc<T>;
-type Parent<T> = RefCell<Weak<T>>;
-type Children<T> = RefCell<Vec<Rc<T>>>;
+type GroupRef = Arc<Group>;
+type Parent = RefCell<Weak<Group>>;
+type Children = RefCell<Vec<GroupRef>>;
 
 #[derive(Clone, Debug)]
-pub struct Group<T> {
-    pub props: T,
-    pub parent: Parent<T>,
-    pub shapes: Children<T>,
+pub struct Group {
+    pub val: ShapeBox,
+    pub parent: Parent,
+    pub shapes: Children,
 }
 
-impl<T> Group<T> {
-    fn from_shape(shape: Shape3D) -> Group<T> {
-        Group {
-            props: shape,
+impl Group {
+    fn from_shape(shape: ShapeBox) -> GroupRef {
+        let g = Group {
+            val: shape.clone(),
             parent: RefCell::new(Weak::new()),
             shapes: RefCell::new(Vec::new()),
-        }
-    }
-
-    fn add_child(&self, shape: Shape3D) {
-        let g = Group::from_shape(shape);
-        // `child_node.parent` is set to weak reference to `parent_node`.
-        *g.parent.borrow_mut() = Rc::downgrade(&self);
-        self.shapes.borrow_mut().push(g.clone())
+        };
+        let g_ref = Arc::new(g);
+        g_ref
     }
 }
 
 impl Shape for Group {
     fn get_id(&self) -> String {
-        format!("group_{}", self.props.id)
+        format!("group_{}", self.val.get_id())
     }
     fn get_transform(&self) -> &Matrix4 {
-        &self.props.transform
+        &self.val.get_transform()
     }
     fn set_transform(&mut self, t: &Matrix4) {
-        self.props.transform = *t;
+        self.val.set_transform(t)
     }
     fn get_material(&self) -> &Material {
-        &self.props.material
+        &self.val.get_material()
     }
     fn set_material(&mut self, m: Material) {
-        self.props.material = m;
+        self.val.set_material(m)
     }
     fn local_intersect(&self, _ray: &Ray) -> Vec<Intersection> {
         //self.saved_ray = ray;
@@ -60,10 +59,16 @@ impl Shape for Group {
 }
 
 // default constructor
-impl Default for Group {
-    fn default() -> Group {
-        Group::from_shape(Shape3D::default())
-    }
+fn default_group() -> GroupRef {
+    Group::from_shape(Box::new(test_shape()))
+}
+
+fn add_child(parent: &GroupRef, shape: ShapeBox) {
+    // Make a GroupRef
+    let g = Group::from_shape(shape);
+    // `child_node.parent` is set to weak reference to `parent_node`.
+    *g.parent.borrow_mut() = Arc::downgrade(&parent);
+    parent.shapes.borrow_mut().push(g.clone())
 }
 
 #[cfg(test)]
@@ -73,21 +78,25 @@ mod tests {
 
     #[test]
     fn transform_is_identity() {
-        let g = Group::default();
+        let g = default_group();
         assert_eq!(*g.get_transform(), Matrix4::identity());
     }
 
     #[test]
     fn default_parent_is_empty() {
-        let g = Group::default();
+        let g = default_group();
         assert!(g.parent.borrow().upgrade().is_none());
         assert!(g.shapes.borrow().is_empty())
     }
 
     #[test]
     fn adding_child() {
-        let g = Group::default();
+        let g = default_group();
         let s = test_shape();
-        g.add_child(&s);
+        add_child(&g, Box::new(s.clone()));
+        assert_eq!(g.shapes.borrow().len(), 1);
+        let child = &g.shapes.borrow()[0];
+        assert_eq!(s.get_id(), child.val.get_id());
+        assert!(g.shapes.borrow()[0].parent.borrow().upgrade().is_some());
     }
 }
